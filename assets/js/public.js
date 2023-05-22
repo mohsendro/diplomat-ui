73be65c1e4727d174d8f2b88976ba4ee890ba4a5
mohsendro <mohsendroo@yahoo.com>
2023-05-22 22:51:51 +0330

    first commit 1402/03/01

---
+++ assets/js/public.js
@@ -0,0 +1,451 @@
+// Menu Scripts
+const toggleMenu = document.getElementById("wsnavtoggle");
+const bodyOverflow = document.querySelector("body");
+toggleMenu.addEventListener("click", overflowHidden);
+
+function overflowHidden() {
+    if (bodyOverflow.classList.contains("overflow")) {
+        bodyOverflow.classList.remove("overflow");
+        bodyOverflow.classList.add("none-overflow");
+    } else {
+        bodyOverflow.classList.remove("none-overflow");
+        bodyOverflow.classList.add("overflow");
+    }
+}
+
+// Menu Scrool Down Coloe Change Scripts
+window.onload = function () {
+
+    window.onscroll = function () {
+        scrollFunction();
+        // console.log(document.body.scrollTop);
+        // console.log(document.documentElement.scrollTop);
+    };
+
+    function scrollFunction() {
+        if (window.matchMedia('screen and (min-width: 1400px)').matches) {
+
+            if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+
+                let bigLogo = document.getElementsByClassName("big-logo")[0];
+                let headerLogo = document.getElementsByClassName("header-logo")[0];
+                let headerNav = document.getElementsByClassName("wsmenu")[0];
+                bigLogo.style.cssText = "display: none;";
+                headerLogo.style.cssText = "width: 90px;";
+                headerNav.style.cssText = "margin-right: 30px;";
+
+            } else {
+
+                let bigLogo = document.getElementsByClassName("big-logo")[0];
+                let headerLogo = document.getElementsByClassName("header-logo")[0];
+                let headerNav = document.getElementsByClassName("wsmenu")[0];
+                bigLogo.style.cssText = "display: block;";
+                headerLogo.style.cssText = "width: 0px;";
+                headerNav.style.cssText = "margin-right: 0;";
+
+            }
+
+        } else {
+
+            let headerNav = document.getElementsByClassName("wsmenu")[0];
+            headerNav.style.cssText = "margin-right: 0;";
+        }
+    }
+
+}
+
+// Filtering Canvas Scripts
+window.addEventListener('resize', function () {
+
+    if (window.matchMedia('screen and (max-width: 1399.98px)').matches) {
+
+        let filterButton = document.getElementsByClassName('filter-canvas-btn');
+        filterButton[0].addEventListener("click", filterButtonStyles);
+
+        function filterButtonStyles() {
+
+            let filterCanvas = document.getElementsByClassName('filter-canvas');
+            filterCanvas[0].classList.toggle("filter-canvas-show")
+
+        }
+
+    } else {
+
+        let filterCanvas = document.getElementsByClassName('filter-canvas');
+        filterCanvas[0].classList.remove("filter-canvas-show")
+
+    }
+
+});
+
+// Swiper Scripts
+var adsSwiper = new Swiper(".adsSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 25,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".ads-swiper-button-next",
+        prevEl: ".ads-swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 25,
+        },
+        1400: {
+            slidesPerView: 5,
+            spaceBetween: 25,
+        },
+    }
+});
+
+var locationSwiper = new Swiper(".locationSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 35,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 35,
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 35,
+        },
+    }
+});
+
+var vipSwiper = new Swiper(".vipSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    effect: "cards",
+    grabCursor: true,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    // navigation: {
+    //     nextEl: ".question-swiper-button-next",
+    //     prevEl: ".question-swiper-button-prev",
+    // },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+});
+
+var questionSwiper = new Swiper(".questionSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    // centeredSlides: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".question-swiper-button-next",
+        prevEl: ".question-swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    thumbs: {
+        swiper: questionthumbnailSwiper,
+    },
+});
+
+var questionthumbnailSwiper = new Swiper(".questionthumbnailSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    // centeredSlides: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    navigation: {
+        nextEl: ".questionthumbnail-swiper-button-next",
+        prevEl: ".questionthumbnail-swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    thumbs: {
+        swiper: questionSwiper,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        },
+    }
+});
+
+var blogSwiper = new Swiper(".blogSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 15,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        },
+    }
+});
+
+var advertisingSwiper = new Swiper(".advertisingSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 15,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 15,
+        },
+        1400: {
+            slidesPerView: 5,
+            spaceBetween: 15,
+        },
+    }
+});
+
+var categorymainSwiper = new Swiper(".categorymainSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    }
+});
+
+// lightGallery Scripts
+lightGallery(document.querySelector(".advertisingsingle .gallery"), {
+    plugins: [
+        lgZoom,
+        lgAutoplay,
+        // lgComment,
+        lgFullscreen,
+        // lgHash,
+        lgPager,
+        lgRotate,
+        // lgShare,
+        lgThumbnail,
+        lgVideo,
+        // lgMediumZoom,
+    ],
+});
+
+
+// Counter Scripts 
+// jQuery(document).ready(function ($) {
+
+// $('.count').counterUp({
+// delay: 10,
+// time: 1000
+// });
+
+// });
+
+// Video Scripts
+function vipVideo($number) {
+
+    const video = document.getElementById("video-vip" + [$number]);
+    const circlePlayButton = document.getElementById("circle-play-b" + [$number]);
+    const videoContent = document.getElementById("video-content" + [$number]);
+
+    // circlePlayButton.addEventListener("click", togglePlay);
+    // function togglePlay() {
+    if (video.paused || video.ended) {
+        video.play();
+    } else {
+        video.pause();
+    }
+    // }
+
+    video.removeAttribute('controls');
+    video.addEventListener("playing", function () {
+        circlePlayButton.style.opacity = 0;
+        videoContent.style.opacity = 0;
+        video.setAttribute("controls", "true");
+
+    });
+    video.addEventListener("pause", function () {
+        circlePlayButton.style.opacity = 1;
+        videoContent.style.opacity = 1;
+        video.removeAttribute('controls');
+    });
+
+}
---
+++ footer.php
@@ -0,0 +1,274 @@
+  <!-- Footer Start -->
+  <section id="footer" class="container-fluid footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-xxl-3 d-flex flex-row flex-wrap justify-content-start align-items-start align-content-start column">
+          <img src="assets/img/global/logo-footer.svg" alt="دیپلمات" class="logo">
+          <p class="desc">
+            گروه مشاورین املاک دیپلمات الهیه در سال ۱۳۹۱ با مدیریت آقای یوسف ارسطو در منطقه یک شمیرانات (فرشته) تاسیس گردید. مجموعه دیپلمات الهیه یکی از موفق ترین شرکت های مهندسی فروش و کلیه امور ملکی و پروژه های لوکس در ایران می باشد که به صورت فوق حرفه ای بطور روزانه با افکار و دغدغه های مشتریان، مهندسان و بسیاری از اقشار دیگر در تعامل بوده است. این مجموعه مفتخر است که با در اختیار داشتن کارشناسان مجرب با تحصیلات عالیه در کلیه امور ملکی در منطقه یک شمیرانات آماده ارائه خدمات به شما عزیزان می باشد.
+          </p>
+        </div>
+        <div class="col-12 col-md-6 col-xxl-3 d-flex flex-row flex-wrap justify-content-start align-items-start align-content-start column">
+          <div class="title">
+            <span>ارتباط با ما</span>
+          </div>
+          <div class="info">
+            <div class="contact">
+              <div class="icon">
+                <img src="assets/img/global/icon/pin.svg" alt="address">
+                <img src="assets/img/global/icon/phone.svg" alt="phone">
+              </div>
+              <div class="access">
+                <div class="address">
+                  <div class="head">آدرس :</div>
+                  <span>الهیه، تقاطع شریفی منش و فیاضی، پلاک 3</span>
+                </div>
+                <div class="tel">
+                  <span>تلفن:</span>
+                  <a href="tel:+982122051010"><span><span>۲۱</span><strong>۲۲۰۵۱۰۱۰</strong><span>(+۹۸)</span></span></a>
+                </div>
+              </div>
+            </div>
+            <div class="contact email">
+              <div class="icon">
+                <img src="assets/img/global/icon/email.svg" alt="address">
+              </div>
+              <div class="access">
+                <div class="email">
+                  <div class="head">ایمیل:</div>
+                  <a href="mailto:diplomatelahiye@gmail.com"><span>diplomatelahiye@gmail.com</span></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-xxl-3 d-flex flex-row flex-wrap justify-content-start align-items-start align-content-start column">
+          <div class="title">
+            <span>بخش های مهم</span>
+          </div>
+          <div class="links">
+            <ul>
+              <li><a href="#">درباره ما</a></li>
+              <li><a href="#">محصولات ما</a></li>
+              <li><a href="#">کناف</a></li>
+              <li><a href="#">سقف کاذب</a></li>
+              <li><a href="#">پشتیبانی</a></li>
+              <li><a href="#">مشتریان</a></li>
+              <li><a href="#">شیوه پرداخت</a></li>
+            </ul>
+            <ul>
+              <li><a href="#">درباره ما</a></li>
+              <li><a href="#">محصولات ما</a></li>
+              <li><a href="#">کناف</a></li>
+              <li><a href="#">سقف کاذب</a></li>
+              <li><a href="#">پشتیبانی</a></li>
+              <li><a href="#">مشتریان</a></li>
+              <li><a href="#">شیوه پرداخت</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-12 col-xxl-3 d-flex flex-row flex-wrap justify-content-start align-items-start align-content-start column">
+          <div class="title">
+            <span>خبرنامه</span>
+          </div>
+          <form action="/" method="post" class="subscribe">
+            <input type="email" name="email" id="email" placeholder="Your Email">
+            <!-- <input type="submit" value="عضویت"> -->
+          </form>
+          <span class="email-desc">جهت دریافت  اطلاعات و کاتالوگ محصولات در سایت عضو شوید</span>
+          <ul class="socials">
+            <li><a href="#"><img src="assets/img/global/icon/telegram2.svg" alt="telegram"></a></li>
+            <li><a href="#"><img src="assets/img/global/icon/instagram2.svg" alt="instagram"></a></li>
+            <li><a href="#"><img src="assets/img/global/icon/whatsapp2.svg" alt="whatsapp"></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 column">
+          <div class="copyright">
+            <span class="diplomat">تمامی حقوق این سایت برای <strong> املاک دیپلمات</strong> محفوظ می باشد</span>
+            <span class="novin">طراحی سایت توسط<a href="https://novinmarketing.com/" target="_blank"> نوین مارکتینگ</a></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Footer End -->   
+
+
+  <!-- bootstrap-v5 Js -->
+  <script src="assets/js/vendor/bootstrap-v5/bootstrap.bundle.min.js"></script>
+  <!-- <script src="assets/js/vendor/bootstrap-v5/popper.min.js"></script> -->
+  <!-- <script src="assets/js/vendor/bootstrap-v5/bootstrap.min.js"></script> -->
+  <script src="assets/js/vendor/jquery/jquery.min.js"></script>
+
+  <!-- AOS Js -->
+  <script src="assets/js/vendor/aos/aos.js"></script>
+  <script>AOS.init();</script>
+
+  <!-- Swiper Js -->
+  <script src="assets/js/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Menus Js -->
+  <!-- <script src="assets/js/vendor/webslide/jquery.min.js"></script> -->
+  <script src="assets/js/vendor/webslide/webslidemenu.js"></script>   
+
+  <!-- Filter Js -->
+  <!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script> -->
+  <script src="assets/js/vendor/filter-multi-select/filter-multi-select-bundle.min.js"></script>
+  <script>
+    // Use the plugin once the DOM has been loaded.
+    $(function () {
+      // Apply the plugin 
+      var notifications = $('#notifications');
+      $('#animals').on("optionselected", function(e) {
+        createNotification("selected", e.detail.label);
+      });
+      $('#animals').on("optiondeselected", function(e) {
+        createNotification("deselected", e.detail.label);
+      });
+      function createNotification(event,label) {
+        var n = $(document.createElement('span'))
+          .text(event + ' ' + label + "  ")
+          .addClass('notification')
+          .appendTo(notifications)
+          .fadeOut(3000, function() {
+            n.remove();
+          });
+      }
+      var shapes = $('#shapes').filterMultiSelect({
+        selectAllText: 'all...',
+        placeholderText: 'click to select a shape',
+        filterText: 'search',
+        labelText: 'Shapes',
+        caseSensitive: true,
+      });
+      var cars = $('#cars').filterMultiSelect();
+      var pl1 = $('#programming_languages_1').filterMultiSelect();
+      $('#b1').click((e) => {
+        pl1.enableOption("1");
+      });
+      $('#b2').click((e) => {
+        pl1.disableOption("1");
+      });
+      var pl2 = $('#programming_languages_2').filterMultiSelect();
+      $('#b3').click((e) => {
+        pl2.enable();
+      });
+      $('#b4').click((e) => {
+        pl2.disable();
+      });
+      var pl3 = $('#programming_languages_3').filterMultiSelect({
+        allowEnablingAndDisabling: false,
+      });
+      $('#b5').click((e) => {
+        pl3.enableOption("1");
+      });
+      $('#b6').click((e) => {
+        pl3.disableOption("1");
+      });
+      $('#b7').click((e) => {
+        pl3.enable();
+      });
+      $('#b8').click((e) => {
+        pl3.disable();
+      });
+      var cities = $('#cities').filterMultiSelect({
+        items: [["San Francisco","a"],
+                ["Milan","b",false,true],
+                ["Singapore","c",true],
+                ["Berlin","d",true,true],
+                ],
+      });
+      var colors = $('#colors').filterMultiSelect();
+      var trees = $('#trees').filterMultiSelect({
+        selectionLimit: 3,
+      });
+      $('#jsonbtn1').click((e) => {
+          var b = true;
+          $('#jsonresult1').text(JSON.stringify(getJson(b),null,"  "));
+        });
+        var getJson = function (b) {
+          var result = $.fn.filterMultiSelect.applied
+              .map((e) => JSON.parse(e.getSelectedOptionsAsJson(b)))
+              .reduce((prev,curr) => {
+                prev = {
+                  ...prev,
+                  ...curr,
+                };
+                return prev;
+              });
+          return result;
+        }
+        $('#jsonbtn2').click((e) => {
+          var b = false;
+          $('#jsonresult2').text(JSON.stringify(getJson(b),null,"  "));
+        });
+      $('#form').on('keypress keyup', function(e) {
+        var keyCode = e.keyCode || e.which;
+        if (keyCode === 13) { 
+          e.preventDefault();
+          return false;
+        }
+      });
+    });
+  </script>
+  <script>
+    var rangeOne = document.querySelector('input[name="rangeOne"]'),
+    rangeTwo = document.querySelector('input[name="rangeTwo"]'),
+    outputOne = document.querySelector('.outputOne'),
+    outputTwo = document.querySelector('.outputTwo'),
+    inclRange = document.querySelector('.incl-range'),
+    updateView = function () {
+      if (this.getAttribute('name') === 'rangeOne') {
+        outputOne.innerHTML = this.value;
+        outputOne.style.right = this.value / this.getAttribute('max') * 100 + '%';
+      } else {
+        outputTwo.style.right = this.value / this.getAttribute('max') * 100 + '%';
+        outputTwo.innerHTML = this.value
+      }
+      if (parseInt(rangeOne.value) > parseInt(rangeTwo.value)) {
+        inclRange.style.width = (rangeOne.value - rangeTwo.value) / this.getAttribute('max') * 100 + '%';
+        inclRange.style.right = rangeTwo.value / this.getAttribute('max') * 100 + '%';
+      } else {
+        inclRange.style.width = (rangeTwo.value - rangeOne.value) / this.getAttribute('max') * 100 + '%';
+        inclRange.style.right = rangeOne.value / this.getAttribute('max') * 100 + '%';
+      }
+    };
+
+    document.addEventListener('DOMContentLoaded', function () {
+      updateView.call(rangeOne);
+      updateView.call(rangeTwo);
+      $('input[type="range"]').on('mouseup', function () {
+        this.blur();
+      }).on('mousedown input', function () {
+        updateView.call(this);
+      });
+    });
+  </script>
+
+  <!-- LightGallery Js -->
+  <script src="assets/vendor/lightgallery/lightgallery.min.js"></script>
+  <script src="assets/vendor/lightgallery/plugins/autoplay/lg-autoplay.min.js"></script>
+  <!-- <script src="assets/vendor/lightgallery/plugins/comment/lg-comment.min.js"></script> -->
+  <script src="assets/vendor/lightgallery/plugins/fullscreen/lg-fullscreen.min.js"></script>
+  <!-- <script src="assets/vendor/lightgallery/plugins/hash/lg-hash.min.js"></script> -->
+  <script src="assets/vendor/lightgallery/plugins/mediumZoom/lg-medium-zoom.min.js"></script>
+  <script src="assets/vendor/lightgallery/plugins/pager/lg-pager.min.js"></script>
+  <script src="assets/vendor/lightgallery/plugins/relativeCaption/lg-relative-caption.min.js"></script>
+  <script src="assets/vendor/lightgallery/plugins/rotate/lg-rotate.min.js"></script>
+  <!-- <script src="assets/vendor/lightgallery/plugins/share/lg-share.min.js"></script> -->
+  <script src="assets/vendor/lightgallery/plugins/thumbnail/lg-thumbnail.min.js"></script>
+  <script src="assets/vendor/lightgallery/plugins/video/lg-video.min.js"></script>
+  <script src="assets/vendor/lightgallery/plugins/vimeoThumbnail/lg-vimeo-thumbnail.min.js"></script>
+  <script src="assets/vendor/lightgallery/plugins/zoom/lg-zoom.min.js"></script>
+
+  <!-- Counter Js -->
+  <!-- <script src="assets/js/vendor/counterup//waypoints.min.js"></script>
+  <script src="assets/js/vendor/counterup/jquery.counterup.min.js"></script> -->
+
+  <!-- Diplomat Js -->
+  <script src="assets/js/public.js"></script>
+
+</body>
+</html>
